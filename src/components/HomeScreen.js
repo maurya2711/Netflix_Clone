c841f3158255327bdfa83ced7a2f5f01307ccb53
maurya2711 <sharadmaurya4600@gmail.com>
2021-06-15 12:54:20 +0530

    initial commit

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+import Banner from "./Banner";
+import requests from "./request";
+
+import Row from "./Row";
+
+function HomeScreen() {
+  return (
+    <div className="HomeScreen">
+      <Nav />
+      <Banner />
+      <Row
+        title="Netflix Orignals"
+        fetchUrl={requests.fetchNetflixOrignals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comeddy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAaMIJypuzWEitExCfvcity1Ma2miAZfHk",
+  authDomain: "netflix-clone-b3df5.firebaseapp.com",
+  projectId: "netflix-clone-b3df5",
+  storageBucket: "netflix-clone-b3df5.appspot.com",
+  messagingSenderId: "145352214581",
+  appId: "1:145352214581:web:7b687b9c27dceea4cd097c",
+  measurementId: "G-YFSBM0TK22",
+};
